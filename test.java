3d5d2802782a462166281904ee04f55763134c84
root <root@guestu.sn+9>
2016-10-21 20:35:16 +0000

    bobodioulassou

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ public class test{
 	public static void main(String [] args){
 		System.out.println("hello");
 		System.out.println("line 1");
-		int val+1;
+		int val=0;
+		val+=1;
+		System.out.println("bobo dioulassou");
 	}
 }
