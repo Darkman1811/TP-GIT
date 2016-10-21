2ee0692cbc2b0172a9da0f35c3b8e89b540a1a1b
root <root@guestu.sn+9>
2016-10-21 16:51:27 +0000

    new commit to master

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ public class test{
 	public static void main(String [] args){
 		System.out.println("hello");
 		System.out.println("line 1");
-		int val+1;
+		int val+=1;
+        System.out.println(val);
 	}
 }
