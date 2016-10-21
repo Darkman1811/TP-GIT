95cd237d505d2967c6466d5d2a6abbbdf016671d
root <root@guestu.sn+9>
2016-10-21 20:54:57 +0000

    adding name and age vars

--- test.java
+++ test.java
@@ -2,7 +2,9 @@ public class test{
 	public static void main(String [] args){
 		System.out.println("hello");
 		System.out.println("line 1");
-		int val+=1;
+		int val=1;
         System.out.println(val);
+        String name="Abdoulaye Diaw";
+        int age=30;
 	}
 }
